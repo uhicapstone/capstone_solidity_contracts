4cd054593be52a5a708f33e07087410a7f6dfd68
mo-hak <mohakgupta100@gmail.com>
2024-11-19 22:40:42 +0530

    Added flash loan contracts

---
+++ src/FlashBorrower.sol
@@ -0,0 +1,88 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "openzeppelin-contracts/contracts/interfaces/IERC3156FlashBorrower.sol";
+import "openzeppelin-contracts/contracts/interfaces/IERC20.sol";
+import "openzeppelin-contracts/contracts/interfaces/IERC3156FlashLender.sol";
+
+/**
+ * @title FlashBorrower
+ * @dev Example implementation of the ERC-3156 Flash Borrower interface.
+ */
+contract FlashBorrower is IERC3156FlashBorrower {
+    enum Action {NORMAL, OTHER}
+
+    IERC3156FlashLender public lender;
+
+    
+    error UntrustedLender(address lender);
+    error UntrustedInitiator(address initiator);
+
+    /**
+     * @dev Set the lender during deployment.
+     * @param lender_ The address of the flash lender.
+     */
+    constructor(IERC3156FlashLender lender_) {
+        lender = lender_;
+    }
+
+    /**
+     * @dev ERC-3156 Flash loan callback.
+     * @param initiator The initiator of the loan.
+     * @param token The loan currency.
+     * @param amount The amount of tokens lent.
+     * @param fee The additional amount of tokens to repay.
+     * @param data Arbitrary data structure, intended to contain user-defined parameters.
+     * @return The keccak256 hash of "ERC3156FlashBorrower.onFlashLoan".
+     */
+    function onFlashLoan(
+        address initiator,
+        address token,
+        uint256 amount,
+        uint256 fee,
+        bytes calldata data
+    ) external override returns (bytes32) {
+        // Verify the lender
+        if (msg.sender != address(lender)) {
+            revert UntrustedLender(msg.sender);
+        }
+
+        // Verify the loan initiator
+        if (initiator != address(this)) {
+            revert UntrustedInitiator(initiator);
+        }
+
+        // Decode the action from the data
+        (Action action) = abi.decode(data, (Action));
+
+        // Handle the action
+        if (action == Action.NORMAL) {
+            // Perform a normal operation
+        } else if (action == Action.OTHER) {
+            // Perform another operation
+        }
+
+        // Return the success callback
+        return keccak256("ERC3156FlashBorrower.onFlashLoan");
+    }
+
+    /**
+     * @dev Initiate a flash loan.
+     * @param token The loan currency.
+     * @param amount The amount of tokens to borrow.
+     */
+    function flashBorrow(address token, uint256 amount) public {
+        // Encode the action for the flash loan callback
+        bytes memory data = abi.encode(Action.NORMAL);
+
+        // Calculate the repayment amount
+        uint256 _fee = lender.flashFee(token, amount);
+        uint256 _repayment = amount + _fee;
+
+        // Approve the lender to withdraw the repayment
+        IERC20(token).approve(address(lender), _repayment);
+
+        // Initiate the flash loan
+        lender.flashLoan(this, token, amount, data);
+    }
+}
---
+++ src/FlashLender.sol
@@ -0,0 +1,114 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "openzeppelin-contracts/contracts/interfaces/IERC3156FlashBorrower.sol";
+import "openzeppelin-contracts/contracts/interfaces/IERC20.sol";
+import "openzeppelin-contracts/contracts/interfaces/IERC3156FlashLender.sol";
+import "./InsurancePool.sol";
+
+
+contract FlashLender is IERC3156FlashLender {
+
+    bytes32 public constant CALLBACK_SUCCESS = keccak256("ERC3156FlashBorrower.onFlashLoan");
+    InsurancePool public insurancePool;
+
+    uint256 public fee; // Fee percentage in basis points (bps)
+
+    
+    error TransferFailed(address token, address to, uint256 amount);
+    error CallbackFailed(address receiver);
+    error RepaymentFailed(address token, address from, uint256 amount);
+    error UnsupportedToken(address token);
+
+    /**
+     * @param insurancePool_ The address of the InsurancePool contract.
+     * @param fee_ The percentage of the loan `amount` that needs to be repaid, in addition to `amount`.
+     */
+    constructor(address insurancePool_, uint256 fee_) {
+        insurancePool = InsurancePool(insurancePool_);
+        fee = fee_;
+    }
+
+    /**
+     * @dev Loan `amount` tokens to `receiver`, and takes it back plus a `flashFee` after the callback.
+     * @param receiver The contract receiving the tokens, needs to implement the `onFlashLoan` interface.
+     * @param token The loan currency.
+     * @param amount The amount of tokens lent.
+     * @param data A data parameter to be passed on to the `receiver` for any custom use.
+     */
+    function flashLoan(
+        IERC3156FlashBorrower receiver,
+        address token,
+        uint256 amount,
+        bytes calldata data
+    ) external override returns (bool) {
+        uint256 loanFee = _flashFee(token, amount);
+
+        // Transfer tokens from the InsurancePool
+        if (!insurancePool.transferFunds(token, address(receiver), amount)) {
+            revert TransferFailed(token, address(receiver), amount);
+        }
+
+        // Callback to the receiver
+        if (
+            receiver.onFlashLoan(msg.sender, token, amount, loanFee, data) !=
+            CALLBACK_SUCCESS
+        ) {
+            revert CallbackFailed(address(receiver));
+        }
+
+        // Pull repayment from the receiver
+        uint256 totalRepayment = amount + loanFee;
+        if (
+            !IERC20(token).transferFrom(
+                address(receiver),
+                address(insurancePool),
+                totalRepayment
+            )
+        ) {
+            revert RepaymentFailed(token, address(receiver), totalRepayment);
+        }
+
+        return true;
+    }
+
+    /**
+     * @dev The fee to be charged for a given loan.
+     * @param token The loan currency.
+     * @param amount The amount of tokens lent.
+     * @return The amount of `token` to be charged for the loan, on top of the returned principal.
+     */
+    function flashFee(
+        address token,
+        uint256 amount
+    ) external view override returns (uint256) {
+        return _flashFee(token, amount);
+    }
+
+    /**
+     * @dev Internal function to calculate the fee for a given loan.
+     * @param token The loan currency.
+     * @param amount The amount of tokens lent.
+     * @return The amount of `token` to be charged for the loan, on top of the returned principal.
+     */
+    function _flashFee(
+        address token,
+        uint256 amount
+    ) internal view returns (uint256) {
+        if (!insurancePool.isTokenSupported(token)) {
+            revert UnsupportedToken(token);
+        }
+        return (amount * fee) / 10000;
+    }
+
+    /**
+     * @dev The amount of currency available to be lent.
+     * @param token The loan currency.
+     * @return The amount of `token` that can be borrowed.
+     */
+    function maxFlashLoan(
+        address token
+    ) external view override returns (uint256) {
+        return insurancePool.getAvailableLiquidity(token);
+    }
+}
